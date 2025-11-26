f5a546cf0638d69cc7938808c777cea05ae4993f
James Langdon <corporatelangdon@gmail.com>
2025-11-25 21:30:35 -0500

    add default logger

--- Sources/VLNetworkingClient/Public/Protocols/Logger.swift
+++ Sources/VLNetworkingClient/Public/Protocols/Logger.swift
@@ -7,7 +7,21 @@
 
 import Foundation
 
-public protocol Logger: Actor {
+public protocol Logger: Sendable {
     func log(_ message: String)
     func debug(_ message: String)
+}
+
+extension Logger {
+    public func log(_ message: String) {
+        print("LOG \(message)")
+    }
+    
+    public func debug(_ message: String) {
+        print("DEBUG \(message)")
+    }
+}
+
+public final class DefaultLogger: Logger {
+    public init() {}
 }
